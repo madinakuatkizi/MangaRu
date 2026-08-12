c35cde39b214652de196eaa0c05b7168f8f349e1
madinakuatkizi <thghost399@gmail.com>
2026-08-13 03:28:41 +0500

    Create Color.kt

---
+++ app/src/main/java/com/mangaru/app/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.mangaru.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBackground = Color(0xFF121212)
+val CardBackground = Color(0xFF1E1E1E)
+val PrimaryAccent = Color(0xFFE50914)
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xFFA0A0A0)
