313c404f5562b45f4b441bdcf2c3672853dec568
madinakuatkizi <thghost399@gmail.com>
2026-08-13 06:53:01 +0500

    Update MainActivity.kt

--- app/src/main/java/com/mangaru/app/ui/MainActivity.kt
+++ app/src/main/java/com/mangaru/app/ui/MainActivity.kt
@@ -40,7 +40,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate()
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         createNotificationChannel()
